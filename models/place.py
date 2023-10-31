aa362f78e02621d952e675e4c410d9c784cbc6d1
EJMarkle <129889001+EJMarkle@users.noreply.github.com>
2023-10-31 17:24:50 -0500

    import fix?

--- models/place.py
+++ models/place.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from sqlalchemy import Column, String, Integer, Float
+from sqlalchemy import ForeignKey, Column, String, Integer, Float
 from models.base_model import BaseModel, Base
 
 
--- models/user.py
+++ models/user.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, relationship
 from sqlalchemy import Column, String
 
 
@@ -10,7 +10,6 @@ class User(BaseModel):
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
-    last_name = Column(string(128))
+    last_name = Column(String(128))
 
     places = relationship("Place", backref="user", cascade="all, delete-orphan")
-    
