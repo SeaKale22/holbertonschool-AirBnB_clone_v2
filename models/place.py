df4421cbee2ed388dde098edf0d3d537b8a9bceb
EJMarkle <129889001+EJMarkle@users.noreply.github.com>
2023-10-31 22:09:03 -0500

    line limit fix

--- models/place.py
+++ models/place.py
@@ -3,11 +3,11 @@ from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 
 place_amenity = Table('place_amenity', Base.metadata, 
-                      Column('place_id', String(60), ForeignKey('places.id'),
-                             primary_key=True, nullable=False),
-                      Column('amenity_id', String(60),
-                             ForeignKey('amenities.id'), primary_key=True,
-                             nullable=False)
+                        Column('place_id', String(60), ForeignKey('places.id'),
+                                primary_key=True, nullable=False),
+                        Column('amenity_id', String(60),
+                                ForeignKey('amenities.id'), primary_key=True,
+                                nullable=False)
 )
 
 class Place(BaseModel, Base):
