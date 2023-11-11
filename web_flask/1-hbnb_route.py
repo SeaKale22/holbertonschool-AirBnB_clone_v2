d9151c02b5059d002e35d32c037afb1cc566f379
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-11-11 10:01:32 -0800

    two routes

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Simple Flask web app"""
+from flask import Flask
+
+# create flask app
+app = Flask(__name__)
+
+
+# route for /
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """returns string"""
+    return 'Hello HBNB!'
+
+# route for /hbnb
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """display HBNB"""
+    return 'HBNB'
+
+
+
+# run app on 0.0.0.0, port 5000
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
