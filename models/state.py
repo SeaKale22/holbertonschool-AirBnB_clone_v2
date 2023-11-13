77951495bef5e08167b691c36f2572e54634694c
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-11-13 08:03:17 -0800

    added if statement to check if storeage is Filestorage before cities getter

--- models/state.py
+++ models/state.py
@@ -4,6 +4,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel
 from models.base_model import Base
+from os import environ
 
 
 class State(BaseModel, Base):
@@ -13,12 +14,13 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state", cascade="all, delete-orphan")
 
-    @property
-    def cities(self):
-        """Getter attribute to return the list of City instances with state_id
-        equal to the current State.id for FileStorage."""
-        cities = []
-        for city in storage.all("City").values():
-            if city.state_id == self.id:
-                cities.append(city)
-        return cities
+    if environ.get('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            """Getter attribute to return the list of City instances with state_id
+            equal to the current State.id for FileStorage."""
+            cities = []
+            for city in storage.all("City").values():
+                if city.state_id == self.id:
+                    cities.append(city)
+            return cities
