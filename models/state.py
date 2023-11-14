a31589ef10a0d8d984cf40f72b83b7285300e646
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-11-14 10:16:50 -0600

    jump start

--- models/state.py
+++ models/state.py
@@ -1,26 +1,20 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from sqlalchemy import Column, Integer, String, ForeignKey
+""" holds class State"""
+import models
+from models.base_model import BaseModel, Base
+from models.city import City
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-from models.base_model import BaseModel
-from models.base_model import Base
-from os import environ
 
 
 class State(BaseModel, Base):
-    """ State class """
-    name = ""
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete-orphan")
-
-    if environ.get('HBNB_TYPE_STORAGE') != 'db':
-        @property
-        def cities(self):
-            """Getter attribute to return the list of City instances with state_id
-            equal to the current State.id for FileStorage."""
-            cities = []
-            for city in storage.all("City").values():
-                if city.state_id == self.id:
-                    cities.append(city)
-            return cities
+    """Representation of state """
+    if models.storage_t == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
+    else:
+        name = ""
+	cites = []
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,5 @@
--- prepares a MySQL
--- New dataase and user with PRIVILEGES
+-- Create hbnh_test_db
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+CREATE USER IF NOT EXISTS 'hbnb_test' @'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test' @'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test' @'localhost';
