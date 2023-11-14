a7657ecb96a1745f9dbc3471a04734b7a7925574
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-11-14 09:27:19 -0800

    tries to show html with list of states and template

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Simple Flask web app"""
+from flask import Flask, render_template
+from models import storage
+
+# create flask app
+app = Flask(__name__)
+
+@app.teardown_appcontext
+def teardown_storage(exception):
+    """teardown method"""
+    storage.close()
+
+# route for /
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Shows html of states list"""
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda x: x.name)
+    return render_template('states_list.html', states=sorted_states)
+
+
+
+# run app on 0.0.0.0, port 5000
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
