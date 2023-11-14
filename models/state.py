d5ecd4717907489511533de0bf6439ef1df1ed1d
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-11-14 08:56:26 -0800

    import storage from models

--- models/state.py
+++ models/state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ holds class State"""
 import models
+from models import storage
 from models.base_model import BaseModel, Base
 from models.city import City
 from os import getenv
